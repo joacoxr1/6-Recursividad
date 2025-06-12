dceb38a2493a05d83b10ef91e9882cd098891b39
Joaquin Gabriel Rodriguez <106762436+joacoxr1@users.noreply.github.com>
2025-06-12 04:00:45 -0300

    Update Ejercicio8.py

--- Ejercicio8.py
+++ Ejercicio8.py
@@ -5,7 +5,7 @@ def contar_digito(num, digito):
         return 1 + contar_digito(num // 10, digito)
     else:
         return contar_digito(num // 10, digito)
-
+        
 print(contar_digito(12233421, 2)) 
 print(contar_digito(5555, 5))     
 print(contar_digito(123456, 7))    
